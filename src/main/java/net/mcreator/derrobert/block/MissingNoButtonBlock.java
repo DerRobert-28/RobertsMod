617404ba8669c0cceeff3f06a9bfbdf990f9a3e7
Der Robert <erictainment@live.de>
2023-09-15 22:49:59 +0200

    update

--- src/main/java/net/mcreator/derrobert/block/MissingNoButtonBlock.java
+++ src/main/java/net/mcreator/derrobert/block/MissingNoButtonBlock.java
@@ -1,6 +1,7 @@
 
 package net.mcreator.derrobert.block;
 
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
@@ -8,11 +9,17 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.ButtonBlock;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
+
+import net.mcreator.derrobert.procedures.MissingNoButtonClickProcedure;
 
 import java.util.List;
 import java.util.Collections;
@@ -35,5 +42,19 @@ public class MissingNoButtonBlock extends ButtonBlock {
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
 		return Collections.singletonList(new ItemStack(this, 1));
+	}
+
+	@Override
+	public InteractionResult use(BlockState blockstate, Level world, BlockPos pos, Player entity, InteractionHand hand, BlockHitResult hit) {
+		super.use(blockstate, world, pos, entity, hand, hit);
+		int x = pos.getX();
+		int y = pos.getY();
+		int z = pos.getZ();
+		double hitX = hit.getLocation().x;
+		double hitY = hit.getLocation().y;
+		double hitZ = hit.getLocation().z;
+		Direction direction = hit.getDirection();
+		MissingNoButtonClickProcedure.execute(entity);
+		return InteractionResult.SUCCESS;
 	}
 }
